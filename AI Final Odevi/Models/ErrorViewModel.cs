7a1a649e795290670224f469b15927b86920f5f7
ercha <vxc67973@gmail.com>
2022-01-18 16:19:01 +0300

    Add project files.

---
+++ AI Final Odevi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AI_Final_Odevi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
