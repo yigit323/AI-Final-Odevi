f67a44be1556262dbc03023edbdae85484a8877f
Yiğit Kalkan <b.k4lkan@yandex.com>
2022-01-19 13:41:37 +0300

    Startup Değişiklik

--- AI Final Odevi/Startup.cs
+++ AI Final Odevi/Startup.cs
@@ -33,14 +33,11 @@ namespace AI_Final_Odevi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
@@ -48,10 +45,9 @@ namespace AI_Final_Odevi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
+    }
     }
 }
